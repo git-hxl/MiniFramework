5a3dd7cbba95824a28aea96380f65ba8a47bebaa
公司 <1413698201@qq.com>
2019-04-17 11:06:03 +0800

    update 4.17

--- Assets/MiniFramework/2.UIModule/Tools/Scripts/Joystick.cs
+++ Assets/MiniFramework/2.UIModule/Tools/Scripts/Joystick.cs
@@ -13,11 +13,11 @@ namespace MiniFramework
         OnDrag,
         OnEndDrag,
     }
-    public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
+    public class Joystick : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
         public RectTransform Rocker;//摇杆
         public RectTransform Base;//底座
-        public RectTransform Arrow;//箭头
+        public RectTransform Arrow;
         public float MaxDistance;//摇杆最大移动距离
         public Action OnBeginDragHandler;//摇杆开始拖拽事件
         public Action<Vector2> OnDragHandler;//摇杆拖拽中事件
@@ -25,8 +25,22 @@ namespace MiniFramework
         public JoyStickState CurState;//摇杆当前状态
         public Vector2 RockerDir { get { return Rocker.localPosition.normalized; } }//摇杆方向
         private Vector2 rockerPos;
+
+        private int pointerID = -1;
         public void OnBeginDrag(PointerEventData eventData)
         {
+            if (pointerID == -1)
+            {
+                pointerID = eventData.pointerId;
+            }
+            else
+            {
+                return;
+            }
+            Base.gameObject.SetActive(true);
+            Vector2 position;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, eventData.position, eventData.enterEventCamera, out position);
+            Base.localPosition = position;
             rockerPos = Rocker.localPosition;
             if (OnBeginDragHandler != null)
             {
@@ -36,7 +50,10 @@ namespace MiniFramework
         }
         public void OnDrag(PointerEventData eventData)
         {
-            Base.gameObject.SetActive(true);
+            if (eventData.pointerId != pointerID)
+            {
+                return;
+            }
             rockerPos += eventData.delta;
             float distance = Vector2.Distance(rockerPos, Vector2.zero);
             if (distance > MaxDistance)
@@ -46,7 +63,7 @@ namespace MiniFramework
             Rocker.localPosition = rockerPos;
             if (Arrow != null)
             {
-                Arrow.localPosition = Rocker.localPosition.normalized * distance;
+                Arrow.localPosition = Rocker.localPosition.normalized * MaxDistance;
                 Arrow.up = Rocker.localPosition.normalized;
             }
             if (OnDragHandler != null)
@@ -58,25 +75,18 @@ namespace MiniFramework
 
         public void OnEndDrag(PointerEventData eventData)
         {
+            if (eventData.pointerId != pointerID)
+            {
+                return;
+            }
             Rocker.localPosition = Vector3.zero;
             if (OnEndDragHandler != null)
             {
                 OnEndDragHandler();
             }
             CurState = JoyStickState.OnEndDrag;
-        }
-
-        public void OnPointerDown(PointerEventData eventData)
-        {
-            Vector2 position;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, eventData.position, eventData.enterEventCamera, out position);
-            Base.localPosition = position;
-        }
-
-        public void OnPointerUp(PointerEventData eventData)
-        {
             Base.gameObject.SetActive(false);
+            pointerID = -1;
         }
     }
-
 }
