bffcecbde657285cb308dac5f14b570ea124bdd0
愤怒的阿凡提 <1413698201@qq.com>
2020-03-01 23:14:54 +0800

    update 202.3.1

--- MiniDemo/Assets/Scripts/Framework/CoreModule/Tools/CameraFollow.cs
+++ MiniDemo/Assets/Scripts/Framework/CoreModule/Tools/CameraFollow.cs
@@ -5,23 +5,28 @@ namespace MiniFramework
     {
         public Transform Target;
         public float Distance = 15;
+        [Range(0, 90)]
         public float Angle = 45;
-        public float LerpSpeed = 20;
+        [Range(0f, 1f)]
+        public float lerpSpeed = 0.5f;
         Vector3 targetPos;
+        Vector3 targetAngle;
         // Use this for initialization
         void Start()
         {
 
         }
-        private void LateUpdate()
+        private void FixedUpdate()
         {
             if (Target != null)
             {
                 targetPos = Target.position - transform.forward * Distance;
 
-                transform.position =   Vector3.MoveTowards(transform.position, targetPos, LerpSpeed * Time.deltaTime);
+                transform.position = Vector3.Slerp(transform.position, targetPos, lerpSpeed);
 
-                transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, new Vector3(Angle, 0, 0), LerpSpeed * Time.deltaTime);
+                targetAngle = new Vector3(Angle, 0, 0);
+                transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetAngle, lerpSpeed);
+
             }
         }
     }
