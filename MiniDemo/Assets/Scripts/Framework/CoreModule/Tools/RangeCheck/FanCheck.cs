eb4be3e587e6d7bcec186d7e041baffa9cae468c
愤怒的阿凡提 <1413698201@qq.com>
2020-03-01 23:36:54 +0800

    update

---
+++ MiniDemo/Assets/Scripts/Framework/CoreModule/Tools/RangeCheck/FanCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MiniFramework
+{
+    public class FanCheck : MonoBehaviour
+    {
+		public float angle=30;
+		public float  radius=2;
+		public Transform target;
+
+        // Update is called once per frame
+        void Update()
+        {
+			Debug.Log(RangeCheck.FanCheck(transform,target.position,angle,radius));
+        }
+    }
+
+}
